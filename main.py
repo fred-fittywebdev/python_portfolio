c70afcaecc5cd5c413f9c048a917b6c7ebfdca54
fred-fittywebdev <contact@fittywebdev.fr>
2023-06-02 09:26:00 +0200

    Create columns, image and description of myself.

---
+++ main.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col_1, col_2 = st.columns(2)
+
+with col_1:
+    st.image("images/photo.png")  # width=500
+
+with col_2:
+    st.title("Frédéric Guerra")
+    content = """
+    Développeur intégrateur web junior. Créatif et innovant, avoir travailler
+    pendant 1 an au sein d'un groupe international comme Boardriders me
+    permet de créer des sites web et des applications pour des clients de premier
+    plan aux demandes et visions complexes. Doté d'excellentes compétences en
+    matière d'organisation, de planification et de travail en équipe. Ouvert à de
+    nouveaux challenges je souhaite vous apporter mon expertise en
+    développement web.
+    """
+    st.info(content)
