b3800c182d8742567baf66dc07b98ff7e87699b7
fred-fittywebdev <contact@fittywebdev.fr>
2023-06-02 20:36:29 +0200

    Add send email function

--- pages/Contact.py
+++ pages/Contact.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.header("Contactez-moi !")
+
+with st.form(key="email_forms"):
+    user_email = st.text_input("Votre Adresse Email")
+    message = st.text_area("Votre message")
+    submit_btn = st.form_submit_button("Envoyer")
+
+    if submit_btn:
+        print("send")
