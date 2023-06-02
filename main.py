f7f5c3502902d5d2d11da585592363e677650e5e
fred-fittywebdev <contact@fittywebdev.fr>
2023-06-02 13:57:58 +0200

    Add description, images and links to the projects

--- main.py
+++ main.py
@@ -27,14 +27,20 @@ Below you can find some of the apps i have built in Python. Feel free to contact
 
 st.write(content_2)
 
-col_3, col_4 = st.columns(2)
+col_3, empty_col,  col_4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 with col_3:
     for index, row in df[:10].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image('images/' + row['image'])
+        st.write(f'[Code source]({row["url"]})')
 
 with col_4:
     for index, row in df[10:].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image('images/' + row['image'])
+        st.write(f'[Code source]({row["url"]})')
 
