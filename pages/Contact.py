181d88ead1f5f81588cce8badfac65344692dbb4
fred-fittywebdev <contact@fittywebdev.fr>
2023-06-03 17:08:43 +0200

    Send email using the form on contact page.

--- pages/Contact.py
+++ pages/Contact.py
@@ -1,11 +1,19 @@
 import streamlit as st
+from send_email import send_email
 
 st.header("Contactez-moi !")
 
 with st.form(key="email_forms"):
     user_email = st.text_input("Votre Adresse Email")
-    message = st.text_area("Votre message")
+    raw_message = st.text_area("Votre message")
+    message = f"""\
+Sujet: Nouvel email de {user_email}
+
+From: {user_email}
+{raw_message}
+"""
     submit_btn = st.form_submit_button("Envoyer")
 
     if submit_btn:
-        print("send")
+        send_email(message)
+        st.success("Votre email a bien été envoyé. Merci.")
--- send_email.py
+++ send_email.py
@@ -1,22 +1,17 @@
 import smtplib
 import ssl
 
-host = "smtp.gmail.com"
-port = 465
 
-username = "portfolio.py.fred@gmail.com"
-password = "xdrrkirgjdzmifny"
-receiver = "loulou.fg@gmail.com"
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "portfolio.py.fred@gmail.com"
+    password = "xdrrkirgjdzmifny"
+    receiver = "loulou.fg@gmail.com"
+    context = ssl.create_default_context()
 
-context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
 
-message = """\
-Subject: Contact from the portfolio.
-Hello!
-How are you today?
-Ciao !
-"""
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
 
